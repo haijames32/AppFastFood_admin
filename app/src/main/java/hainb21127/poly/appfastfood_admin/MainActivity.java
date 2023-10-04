330d0ace1742a5a4d1f59722879357537eac88ae
haijames32 <feckhai2003@gmail.com>
2023-10-04 20:13:14 +0700

    first

---
+++ app/src/main/java/hainb21127/poly/appfastfood_admin/MainActivity.java
@@ -0,0 +1,14 @@
+package hainb21127.poly.appfastfood_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppFastFood_admin"
+include(":app")
